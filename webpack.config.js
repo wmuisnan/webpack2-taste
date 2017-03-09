da1312b30a766c3377bef29282529a6cd7e0215e
wmu <wmuisnan@gmail.com>
2017-03-10 00:31:22 +0800

    Code Splitting - Using import()   1. import() as a split-point   2. babel, modules: false, syntax-dynamic-import;   3. publicPath: 'dist/'

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,41 @@ var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   entry: {
-    main: './app/index.js',
+    main: './app/index-es2017.js',
     // vendor: 'moment'
   },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist/'
   },
   module: {
     rules: [{
-      test: /\.css$/, // 不能加引号
-      use: ['style-loader','css-loader']
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              'es2015',
+              {
+                modules: false
+              }
+            ]
+          ],
+          plugins: ['syntax-dynamic-import']
+        }
+      }]
     }]
+
   }
 }
 
+/*
 
+  1. import() as a split-point
+  2. babel, modules: false, 'syntax-dynamic-import'
+  3. publicPath: 'dist/'
+
+ */
