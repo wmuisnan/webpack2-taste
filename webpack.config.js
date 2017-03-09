768f7838c0e9f885a18142de3673139fe5ae83a5
wmu <wmuisnan@gmail.com>
2017-03-09 23:04:32 +0800

    code splitting   每个入口文件都会包含自己的依赖

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   entry: {
     main: './app/index.js',
-    // vendor2: 'moment' @__1
+    vendor: 'moment'
   },
   output: {
     filename: '[chunkhash].[name].js',
@@ -15,19 +15,9 @@ module.exports = {
       test: /\.css$/, // 不能加引号
       use: ['style-loader','css-loader']
     }]
-  },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor' // @__1
-    })
-  ]
+  }
 }
 
 /*
-
- 1. @__1 两处的名字要一样？
- 2. 删掉一处好像没影响?
-
-
-
+  main 里面也有 moment，因为 moment 是 main 的依赖, 而每个入口文件都会包含自己的依赖
  */
