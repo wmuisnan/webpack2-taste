25de57abb65bb0058621801170089594c84d1ca6
wmu <wmuisnan@gmail.com>
2017-03-09 23:19:51 +0800

    code splitting   入口里没有指定vendor, common chunk 会将webpack的运行环境提为公共部分

--- app/index.js
+++ app/index.js
@@ -1,14 +1,17 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import '../css/theme.css';
 
 
-var moment = require('moment');
-console.log(moment().format());
+// var moment = require('moment');
+// console.log(moment().format());
+
+console.log(Date.now())
+
 function component () {
   var element = document.createElement('div');
 
   /* lodash is required for the next line to work */
-  element.innerHTML = _.join(['Hello','webpack'], ' ');
+  element.innerHTML = 'Hello webpack'  //_.join(['Hello','webpack'], ' ');
 
   return element;
 }
